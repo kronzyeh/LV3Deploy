d18702842f84a8c5ed8af84840c19d7275ba72e8
anas.enj <ana.senj13@gmail.com>
2023-11-20 19:16:35 +0100

    Fix index

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
 // Insert data
-$sql_insert = "INSERT INTO registration_tbl (name, email, date)
+$sql_insert = "INSERT INTO register_tbl (name, email, date)
 VALUES ('$name','$email','$date')";
 if ($conn->query($sql_insert) === TRUE)
 {
